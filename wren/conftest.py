6a7d6dbcef55d60f3bead16dfc38ffe9330bce56
barslmn <barslmn@gmail.com>
2021-01-18 14:25:37 +0300

    cookiecutter commit

---
+++ wren/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from wren.users.models import User
+from wren.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
